7c39cd5518a975103fc38deac7b228530809f1eb
paulyves <lucas.paulyves@gmail.com>
2014-01-19 22:21:12 +0100

    added sample of dialog box

--- src/edu/jdr/DicePaper/UniversMenu.java
+++ src/edu/jdr/DicePaper/UniversMenu.java
@@ -1,7 +1,14 @@
 package edu.jdr.DicePaper;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,8 +18,23 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class UniversMenu extends Activity {
+    private Button createUniverse;
+    private Button loadUniverse;
+    private final int CREAUNIVPOPUP = 1;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universmenu);
+
+        createUniverse = (Button) findViewById(R.id.NewUniverse);
+        loadUniverse = (Button) findViewById(R.id.LoadUniverse);
+
+        createUniverse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MyDialogFragment().show(getFragmentManager(), "MyDialog");
+            }
+        });
     }
+
+
 }
