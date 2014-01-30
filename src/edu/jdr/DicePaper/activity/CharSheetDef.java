c455bacc3f1098f6d1da41208d5a51ed2a5ac617
Mario <mariojothy@gmail.com>
2014-01-30 15:31:59 +0100

    temporary commit to pull

--- src/edu/jdr/DicePaper/activity/CharSheetDef.java
+++ src/edu/jdr/DicePaper/activity/CharSheetDef.java
@@ -15,12 +15,17 @@ import edu.jdr.DicePaper.R;
 public class CharSheetDef extends Activity {
     private String universeName;
     private Button addCharac;
+    private Button addJauge;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.char_sheet_def);
+
         universeName = getIntent().getExtras().getString("universeName");
         TextView title = (TextView) findViewById(R.id.univTitle);
         title.setText(universeName);
+
         addCharac = (Button) findViewById(R.id.addCharac);
+
+        addJauge = (Button) findViewById((R.id.addJauge));
     }
 }
