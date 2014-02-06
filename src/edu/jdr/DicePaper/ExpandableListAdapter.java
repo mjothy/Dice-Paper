d5ef9ccdf87e0962e0b565c0c2d3602d902c6f24
Mario <mariojothy@gmail.com>
2014-02-06 17:29:40 +0100

    Custom adapter for expendable lists and MasterLists

---
+++ src/edu/jdr/DicePaper/ExpandableListAdapter.java
@@ -0,0 +1,111 @@
+package edu.jdr.DicePaper;
+
+/**
+ * Created by mario on 06/02/14.
+ */
+
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+import edu.jdr.DicePaper.models.table.MasterListe;
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context _context;
+    private List<MasterListe> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<MasterListe, List<MasterListe>> _listDataChild;
+
+    public ExpandableListAdapter(Context context, List<MasterListe> listDataHeader,
+                                 HashMap<MasterListe, List<MasterListe>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosititon) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .get(childPosititon);
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, final int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+
+        final String childText = getChild(groupPosition, childPosition).toString();
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_child, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.childs);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition))
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String headerTitle = getGroup(groupPosition).toString();
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_component, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.components);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
