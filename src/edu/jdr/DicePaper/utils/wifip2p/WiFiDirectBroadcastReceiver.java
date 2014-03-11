a3e81de1e7b36bbf96123d00b8fabf79f513e71a
Paul-Yves <lucas.paulyves@gmail.com>
2014-03-11 13:53:13 +0100

    partial commit for wifi test

---
+++ src/edu/jdr/DicePaper/utils/wifip2p/WiFiDirectBroadcastReceiver.java
@@ -0,0 +1,47 @@
+package edu.jdr.DicePaper.utils.wifip2p;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
+
+/**
+ * Created by paulyves on 3/7/14.
+ */
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private Activity mActivity;
+
+    public WiFiDirectBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel,
+                                       Activity activity) {
+        super();
+        this.mManager = manager;
+        this.mChannel = channel;
+        this.mActivity = activity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                // Wifi P2P is enabled
+                Toast.makeText(mActivity, "wifi p2p activé", Toast.LENGTH_SHORT).show();
+            } else {
+                // Wi-Fi P2P is not enabled
+                Toast.makeText(mActivity, "erreur : wifi p2p non activé", Toast.LENGTH_SHORT).show();
+            }
+        }else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            // Respond to new connection or disconnections
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+
+}
